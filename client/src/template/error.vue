3b6607334a17288fbadba92207b6906f0ba79b6a
SamGasper <ismoil.zokirov@axtechnology.uz>
2024-10-20 02:13:31 +0500

    feat: ngix error page

---
+++ client/src/template/error.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import { NConfigProvider, NGlobalStyle, darkTheme } from 'naive-ui';
+</script>
+
+<template>
+  <NConfigProvider :theme="darkTheme">
+    <div class="error-template">
+      <slot />
+    </div>
+    <NGlobalStyle />
+  </NConfigProvider>
+</template>
+
+<style lang="scss" scoped>
+  .error-template {
+    width: 100%;
+    height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+</style>
---
+++ client/src/views/Error.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import { NResult } from 'naive-ui';
+</script>
+
+<template>
+  <NResult
+    status="418"
+    title="Упс!"
+    description="Сервис временно не доступен"
+  />
+</template>
