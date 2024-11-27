537e963d092e0be148494949e14cd805d9e5bd1b
SamGasper <ismoil.zokirov@axtechnology.uz>
2024-11-27 23:19:39 +0500

    fix: yt-dlp binary

--- server/src/shared/youtube-dl/index.ts
+++ server/src/shared/youtube-dl/index.ts
@@ -1,4 +1,4 @@
 import YoutubeDlWrap from 'yt-dlp-wrap'
 
-export const youtubeDl = new YoutubeDlWrap(`${__dirname}/binary/yt-dlp_test`)
+export const youtubeDl = new YoutubeDlWrap(`${__dirname}/binary/yt-dlp`)
 
